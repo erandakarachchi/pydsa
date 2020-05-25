24e2592eac89deacf919d4cf6e23231afc5bb28d
erandakarachchi <erandakarachchi@gmail.com>
2020-05-25 07:45:25 +0530

    reverse insertion sort

---
+++ algo/nonincreasing_insertion_sort.py
@@ -0,0 +1,11 @@
+def nonincreasing_insertion_sort(number_list):
+    for key_idx in range(1, len(number_list)):
+        key = number_list[key_idx]
+        sorted_idx = key_idx -1
+        while sorted_idx>0 and number_list[sorted_idx] < key:
+            number_list[sorted_idx+1] = number_list[sorted_idx]
+            sorted_idx -=1
+        number_list[sorted_idx+1] = key
+    return(number_list)
+
+print(nonincreasing_insertion_sort([5,2,4,6,1,3]))
